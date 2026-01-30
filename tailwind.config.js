7c20abf1efd55fc91e3821a5f7d928495ffa7769
Harshal Kulkarni <harshal.kulkarni@gmail.com>
2026-01-30 12:53:12 +0530

    fix: include index.css and public/index.html in Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.css",
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
